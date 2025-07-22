3cd1c5cefac57bb32007c46f07c6305148140c28
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-07-22 16:02:07 +0300

    fix: update script to use input website for DNS queries

--- script.sh
+++ script.sh
@@ -7,25 +7,25 @@ website=$1
 echo "-----$website-----"
 
 echo "Nameserver"
-nameserver=$(dig kosakymm.com NS +short)
+nameserver=$(dig $website NS +short)
 echo $nameserver
 
 echo "IPv4"
-a=$(dig kosakymm.com A +short)
+a=$(dig $website A +short)
 echo $a
 
 echo "IPv6"
-aaaa=$(dig kosakymm.com AAAA +short)
+aaaa=$(dig $website AAAA +short)
 echo $aaaa
 
 echo "MX"
-mx=$(dig kosakymm.com MX +short)
+mx=$(dig $website MX +short)
 echo $mx
 
 echo "TXT"
-txt=$(dig kosakymm.com TXT +short)
+txt=$(dig $website TXT +short)
 echo $txt
 
 echo "CNAME"
-cname=$(dig www.blogger.com CNAME +short)
+cname=$(dig $website CNAME +short)
 echo $cname
