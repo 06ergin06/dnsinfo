cf4e2585a882ba072f8ea404140e15145db3a133
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-07-22 15:47:32 +0300

    A record

--- script.sh
+++ script.sh
@@ -10,4 +10,7 @@ echo "Nameserver"
 nameserver=$(nslookup -query=NS $website | grep nameserver | awk '{print $4}')
 echo $nameserver
 
+echo "A"
+a=$(nslookup -query=A kosakymm.com | grep Address |grep -v "#" | awk '{print $2}')
+echo $a
 
